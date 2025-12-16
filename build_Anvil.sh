fe76f9438864a59fe88e381e1aa27a38e71b5ee6
karimali5 <karimalib45@gmail.com>
2025-12-16 16:22:30 -0500

    include build file for Anvil

---
+++ build_Anvil.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# --- Modules ---
+module purge 
+module load intel/19.0.5.281
+module load mvapich2/2.3.6
+module list  
+
+# --- Compilers (use Cray wrappers) ---
+export COMPILER_ID=Intel
+export FC=mpiifort 
+export CC=mpiicc
+export CXX=mpiicpc
+
+export DECOMP_DIR="/anvil/projects/x-atm170028/padeops_setup/dependencies/2decomp_fft"
+export DECOMP2D_INC="${DECOMP_DIR}/include"
+export DECOMP2D_LIB="${DECOMP_DIR}/lib"
+export source="mpi_diag.F90"
+export prog="MPIR3D"
+
+# INTEL
+${FC} -O3 -traceback -g -warn all -qopenmp -I${DECOMP2D_INC} ${source} -L${DECOMP2D_LIB} -l2decomp_fft -o ${prog}
